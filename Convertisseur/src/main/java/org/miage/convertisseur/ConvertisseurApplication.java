66449ef780a7010440897505126c5def42604457
Jérémy Picard <jeremy55200@hotmail.com>
2021-12-13 22:57:59 +0100

    first commit

---
+++ Convertisseur/src/main/java/org/miage/convertisseur/ConvertisseurApplication.java
@@ -0,0 +1,13 @@
+package org.miage.convertisseur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertisseurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertisseurApplication.class, args);
+	}
+
+}
