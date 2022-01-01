cc6e9089dd8397a2f548d892a492ae37aa181408
Jérémy Picard <jeremy55200@hotmail.com>
2022-01-01 18:19:12 +0100

    Added test for deleting "compte" ressource.

--- Banque/src/test/java/org/miage/banque/controllers/ComptesControllerTest.java
+++ Banque/src/test/java/org/miage/banque/controllers/ComptesControllerTest.java
@@ -1,5 +1,6 @@
 package org.miage.banque.controllers;
 
+import io.restassured.response.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
@@ -107,5 +108,49 @@ class ComptesControllerTest {
                 .post("/comptes")
                 .then()
                 .statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    public void deleteCompte() throws JSONException {
+        JSONObject json = new JSONObject()
+                .put("iban", "FR111111111111111111111111114")
+                .put("solde", "100");
+
+        //Create a new entity that we will delete after.
+        Response response =
+        given()
+                .contentType("application/json")
+                .body(json.toString())
+                .when()
+                .post("/comptes")
+                .then()
+                .statusCode(HttpStatus.SC_CREATED)
+                .extract()
+                .response();
+
+        String linkToCompte = response.path("_links.self.href"); //Retrieve the link to the newly created ressource to try to delete it.
+        String id = linkToCompte.substring(linkToCompte.lastIndexOf("/")); //Extract the id of the newly created ressource
+
+        //Delete the newly created ressource.
+        given()
+                .contentType("application/json")
+                .body(json.toString())
+                .when()
+                .delete("/comptes"+id)
+                .then()
+                .statusCode(HttpStatus.SC_NO_CONTENT)
+                .extract()
+                .response();
+
+        // It should be impossible to get the deleted ressource.
+        given()
+                .contentType("application/json")
+                .body(json.toString())
+                .when()
+                .get("/comptes"+id)
+                .then()
+                .statusCode(HttpStatus.SC_NOT_FOUND)
+                .extract()
+                .response();
     }
 }
