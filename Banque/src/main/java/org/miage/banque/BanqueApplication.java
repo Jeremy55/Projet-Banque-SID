75128947d41f5a65ba4c7c972fe13bc73dcae602
Jérémy Picard <jeremy55200@hotmail.com>
2022-01-03 20:45:09 +0100

    Added OpenAPI.

--- Banque/src/main/java/org/miage/banque/BanqueApplication.java
+++ Banque/src/main/java/org/miage/banque/BanqueApplication.java
@@ -1,7 +1,10 @@
 package org.miage.banque;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BanqueApplication {
@@ -10,4 +13,12 @@ public class BanqueApplication {
 		SpringApplication.run(BanqueApplication.class, args);
 	}
 
+	// http://127.0.0.1:8080/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config
+	@Bean
+	public OpenAPI banqueAPI() {
+		return new OpenAPI().info(new Info()
+				.title("Banque API")
+				.version("1.0")
+				.description("Documentation sommaire de API Banque 1.0"));
+	}
 }
