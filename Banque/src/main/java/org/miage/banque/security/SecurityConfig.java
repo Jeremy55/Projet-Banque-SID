df7a33f3f3dbec86f4bf39df0678dd10561e649c
Jérémy Picard <jeremy55200@hotmail.com>
2022-01-08 15:04:39 +0100

    Working JWT Token, can be used to Authenticate and Authorize clients.

--- Banque/src/main/java/org/miage/banque/security/SecurityConfig.java
+++ Banque/src/main/java/org/miage/banque/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package org.miage.banque.security;
 
 import lombok.RequiredArgsConstructor;
 import org.miage.banque.security.filter.CustomAuthenticationFilter;
+import org.miage.banque.security.filter.CustomAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -30,8 +33,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers("/inscription").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/users").hasAnyAuthority("ROLE_CLIENT");
+        http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
