a3bf7ef2998b140a94bffbd20e79b3c196efd2a5
Jérémy Picard <jeremy55200@hotmail.com>
2022-01-16 04:14:02 +0100

    Can make payments to the bank.

--- Commerce/src/main/java/org/miage/commerce/controllers/Paiements.java
+++ Commerce/src/main/java/org/miage/commerce/controllers/Paiements.java
@@ -18,8 +18,45 @@ public class Paiements {
 
     private final BanqueDelegate banqueDelegate;
 
+    @PostMapping(value = "/contact")
+    public String paiementSansContact(@RequestBody PaiementInput paiementInput){
+        paiementInput.setLibelle("Auchan");
+        paiementInput.setCategorie("Auchan - Débit - Sans contact");
+        paiementInput.setPays("France");
+        paiementInput.setDevise("EUR");
+        paiementInput.setContact(true);
+        paiementInput.setOnline(false);
+        paiementInput.setLatitude("2");
+        paiementInput.setLongitude("48");
+        paiementInput.setIBAN_debiteur("FR7630002005501234567890185");
+        return banqueDelegate.callBanquePaiement(paiementInput);
+    }
+
+    @PostMapping(value = "/online")
+    public String paiementOnline(@RequestBody PaiementInput paiementInput){
+        paiementInput.setLibelle("Auchan");
+        paiementInput.setCategorie("Auchan - Débit - Online");
+        paiementInput.setPays("France");
+        paiementInput.setDevise("EUR");
+        paiementInput.setContact(false);
+        paiementInput.setOnline(true);
+        paiementInput.setLatitude("2");
+        paiementInput.setLongitude("48");
+        paiementInput.setIBAN_debiteur("FR7630002005501234567890185");
+        return banqueDelegate.callBanquePaiement(paiementInput);
+    }
+
     @PostMapping
-    public void paiement(@RequestBody PaiementInput paiementInput){
-        banqueDelegate.callBanquePaiement(paiementInput);
+    public String paiement(@RequestBody PaiementInput paiementInput){
+        paiementInput.setLibelle("Auchan");
+        paiementInput.setCategorie("Auchan - Débit");
+        paiementInput.setPays("France");
+        paiementInput.setDevise("EUR");
+        paiementInput.setContact(false);
+        paiementInput.setOnline(false);
+        paiementInput.setLatitude("2");
+        paiementInput.setLongitude("48");
+        paiementInput.setIBAN_debiteur("FR7630002005501234567890185");
+        return banqueDelegate.callBanquePaiement(paiementInput);
     }
 }
